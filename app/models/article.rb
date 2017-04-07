804b0bc3e0e870868631a9c0d15ad33d9dcad569
Ryley Sill <ryleysill93@gmail.com>
2017-04-06 18:13:36 -0700

    added all categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,11 +5,11 @@ class Article < ApplicationRecord
   validates :url, :smmry, :title, :category, :age, :img_url, presence: true
 
   def self.create_articles
-    @articles = Article.fetch_articles
-    @articles.each do |article|
+    articles = Article.fetch_articles
+    articles.shuffle.take(15).each do |article|
       smmry = Article.create_smmry(article['url'])
       Article.create(url: article['url'],
-                     smmry: smmry,
+                     smmry: smmry[1..-1],
                      title: article['title'],
                      category: article['category'],
                      age: article['publishedAt'],
@@ -18,32 +18,44 @@ class Article < ApplicationRecord
   end
 
   def self.fetch_articles
-    sources = ['techcrunch']
-    @all_articles = []
+    sources = ['techcrunch', 'the-economist', 'espn', 'entertainment-weekly', 'associated-press']
+    all_articles = []
     sources.each do |src|
       response = Mechanize.new.get("https://newsapi.org/v1/articles?source=#{src}&apiKey=745723fdc5784cbe8a09c975af39d28e")
       articles = JSON.parse(response.body)['articles']
-      category = nil
-
-      case src
-      when 'techcrunch'
-        category = 'Tech'
-      else
-        category = ""
-      end
-
-      articles.each do |article|
-        article['category'] = category
-      end
-      @all_articles.concat(articles)
+      articles = Article.tag_articles(src, articles)
+      all_articles.concat(articles)
     end
-    @all_articles
+    all_articles
   end
 
   def self.create_smmry(url)
     response = Mechanize.new.get("http://api.smmry.com/?SM_API_KEY=2CA94C7AE9&SM_LENGTH=3&SM_URL=#{url}")
 
     JSON.parse(response.body)['sm_api_content']
+  end
+
+  def self.tag_articles(src, articles)
+    category = nil
+    case src
+    when 'techcrunch'
+      category = 'Tech'
+    when 'the-economist'
+      category = 'Business'
+    when 'espn'
+      category = 'Sports'
+    when 'entertainment-weekly'
+      category = 'Entertainment'
+    when 'associated-press'
+      category = 'Politics'
+    else
+      category = ""
+    end
+
+    articles.each do |article|
+      article['category'] = category
+    end
+    articles
   end
 
 end
